091f3d74fef7253a9c39c4ca1a3e145b031f064b
abhishek-bhurke <abhishekbhurke25@gmail.com>
2025-09-30 02:37:16 +0530

    change

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    API_URL: 'http://reglensapi.techket.shop:8192',
+    API_URL: 'https://reglensapi.techket.shop:8192',
     razorPay_key: 'rzp_test_RLpSbOvcIsoP5h'
 };
