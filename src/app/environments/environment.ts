d94ba4dd2b482a620a92f859c43c5f7bef51c099
abhishek-bhurke <abhishekbhurke25@gmail.com>
2025-10-25 01:48:22 +0530

    change

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    API_URL: 'https://a4936e65e782.ngrok-free.app',
+    API_URL: 'https://tnmapi.techket.shop',
     razorPay_key: 'rzp_test_RLpSbOvcIsoP5h',
     FRONTEND_URL: 'http://localhost:4200'
 };
